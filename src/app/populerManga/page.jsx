b2578b0801bd50385989550828829447b986e0cd
haniffayz <149506011+haniffayz@users.noreply.github.com>
2023-12-27 07:01:37 +0700

    Update page.jsx

--- src/app/populerManga/page.jsx
+++ src/app/populerManga/page.jsx
@@ -1,9 +1,9 @@
 "use client"
 
 import React, { useEffect, useState } from "react";
-import HeaderMenu from "../../components/Utilities/HeaderMenu";
-import Pagination from "../component/Utilities/Pagination";
-import AnimeList from "../component/AnimeList";
+import HeaderMenu from "@/components/Utilities/HeaderMenu";
+import Pagination from "@/components/Utilities/Pagination";
+import AnimeList from "@/components/AnimeList";
 import { getAnimeResponse } from "../libs/api-libs";
 
 const Page = async () => {
